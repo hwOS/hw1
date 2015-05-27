7bf1fd01b443329277fabc622ef23e4ecef5d1db
jon536 <jon-536@mail.ru>
2015-05-27 20:23:58 +0300

    implement runpiped

--- lib/helpers.c
+++ lib/helpers.c
@@ -8,7 +8,18 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <signal.h>
 #define MAX_WORD_SIZE 4098
+
+#define EXPAND_STR(tok) #tok
+#define TO_STR(tok) EXPAND_STR(tok)
+#define print_debug_info() perror(__FILE__": line "TO_STR(__LINE__))
+
+#ifdef DEBUG
+#  define print_debug_and_ret() print_debug_info(); return -1;
+#else
+#  define print_debug_and_ret() return -1;
+#endif
 
 ssize_t read_(int fd, void *buf, size_t count) {
     size_t read_bytes = 0;
@@ -118,49 +129,130 @@ execargs_t construct_execargs(const char* const name, char* const * argv) {
 }
 
 int exec(execargs_t* args) {
-    return spawn(args->name, args->argv);
+    int cpid = fork();
+
+    if (cpid < 0) {
+        print_debug_and_ret();
+    }
+
+    if (cpid == 0) { // child process
+        /*fprintf(stderr, "set default sigint for: %d\n", getpid());*/
+
+        signal(SIGINT, SIG_DFL);
+        if (execvp(args->name, args->argv) < 0) {
+            print_debug_and_ret();
+        }
+    }
+    return cpid;
 }
 
-int runpiped(execargs_t** programs, size_t n) {
+static int* childs_pid;
+static size_t cur_child = 0;
+static size_t cnt_childs;
+static int child_status;
+
+void chld_handler(int signum, siginfo_t* info, void* ignore) {
+    /*fprintf(stderr, "signum = %d\n", signum);*/
+    /*fprintf(stderr, "pid: %d\n", getpid());*/
+    /*fprintf(stderr, "pid from: %d\n", info->si_pid);*/
+    /*for (size_t i = 0; i < cnt_childs; ++i) {*/
+        /*fprintf(stderr, "child_pid[%d] = %d\n", (int) i, childs_pid[i]);*/
+    /*}*/
+    if (signum == SIGCHLD && info->si_pid == childs_pid[cur_child]) {
+        /*fprintf(stderr, "in sigchld before\n");*/
+        waitpid(info->si_pid, &child_status, 0);
+        child_status = !WIFEXITED(child_status);
+        cur_child++;
+        /*fprintf(stderr, "in sigchld after\n");*/
+        return;
+    } else { // SIGCHLD or SIGINT
+        /*fprintf(stderr, "in sigchld kill before\n");*/
+        for (size_t i = cur_child; i < cnt_childs; ++i) {
+            /*fprintf(stderr, "kill %d\n", (int) childs_pid[i]);*/
+            if (kill(childs_pid[i], SIGINT) < 0) {
+                print_debug_info();
+                return;
+            }
+        }
+        cur_child = cnt_childs;
+        signal(SIGCHLD, SIG_IGN);
+        /*fprintf(stderr, "in sigchld kill after\n");*/
+    }
+}
+
+int runpiped(execargs_t** programs, const size_t n) {
+    /*fprintf(stderr, "sigchld = %d\n", SIGCHLD);*/
+    /*fprintf(stderr, "sigint = %d\n", SIGINT);*/
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = chld_handler;
+
+    /* Signals blocked during the execution of the handler. */
+    sigemptyset(&sa.sa_mask);
+    sigaddset(&sa.sa_mask, SIGINT);
+    sigaddset(&sa.sa_mask, SIGCHLD);
+
+    if (sigaction(SIGCHLD, &sa, 0) != 0 || sigaction(SIGINT, &sa, 0) != 0) {
+        print_debug_and_ret();
+    }
+
     int old_stdout = dup(STDOUT_FILENO);
     int old_stdin = dup(STDIN_FILENO);
-    int pipefd[2]; // 0 - read
+
+    int pipefd[2 * (n - 1)];
+    int childs[n];
+    childs_pid = childs;
+    cnt_childs = n;
+    child_status = 0;
 
     for (size_t i = 0; i < n - 1; ++i) {
-        if (pipe(pipefd) < 0 ||
-            dup2(pipefd[1], STDOUT_FILENO) < 0) 
+        if (pipe(pipefd + 2 * i) < 0) {
+            print_debug_and_ret();
+        }
+
+        int out_fd = pipefd[2 * i + 1];
+        if (fcntl(out_fd, F_SETFD, FD_CLOEXEC) == -1) {
+            print_debug_and_ret();
+        }
+
+        int in_fd = pipefd[2 * i];
+        if (dup2(out_fd, STDOUT_FILENO) < 0) {
+            print_debug_and_ret();
+        }
+
+        childs[i] = exec(programs[i]);
+        /*fprintf(stderr, "in_fd = %d; out_fd = %d; childs[i] = %d\n", in_fd, out_fd, childs[i]);*/
+        if (childs[i] < 0) {
+            print_debug_and_ret();
+        }
+
+        if (dup2(in_fd, STDIN_FILENO) < 0) {
+            print_debug_and_ret();
+        }
+    }
+
+    if (dup2(old_stdout, STDOUT_FILENO) < 0 || close(old_stdout) < 0) {
+        print_debug_and_ret();
+    }
+
+    childs[n - 1] = exec(programs[n - 1]);
+
+    if (childs[n - 1] < 0 || dup2(old_stdin, STDIN_FILENO) < 0 || close(old_stdin) < 0) {
+        print_debug_and_ret();
+    }
+
+    /*fprintf(stderr, "before for\n");*/
+    for (size_t i = 0; i < n; ++i) {
+        /*fprintf(stderr, "out_fd = %d; in_fd = %d; childs[i] = %d\n", pipefd[2 * i + 1], pipefd[2 * i], childs[i]);*/
+        waitpid(childs[i], NULL, 0);
+
+        if ((i < n - 1 && close(pipefd[2 * i + 1]) < 0) || 
+           (i > 0 && close(pipefd[2 * i - 2]) < 0 ) || child_status != 0) 
         {
-            perror("runpiped");
-            return -1;
-        }
-
-        if (exec(programs[i]) < 0) {
-            return -1;
-        }
-
-        /*fprintf(stderr, "p[0] = %d, p[1] = %d, stdin = %d, stdout = %d\n", pipefd[0], pipefd[1], STDIN_FILENO, STDOUT_FILENO);*/
-        if (close(pipefd[1]) < 0 ||
-            dup2(pipefd[0], STDIN_FILENO) < 0 ||
-            close(pipefd[0]) < 0) 
-        {
-            perror("runpiped");
-            return -1;
+            print_debug_and_ret();
         }
     }
-
-    if (dup2(old_stdout, STDOUT_FILENO) < 0 ||
-        close(old_stdout) < 0) {
-        perror("runpiped");
-        return -1;
-    }
-
-    exec(programs[n - 1]);
-
-    if (dup2(old_stdin, STDIN_FILENO) < 0 ||
-        close(old_stdin) < 0) {
-        perror("runpiped");
-        return -1;
-    }
+    /*fprintf(stderr, "runpiped terminated\n");*/
 
     return 0;
 }
--- simplesh/simplesh.c
+++ simplesh/simplesh.c
@@ -3,20 +3,28 @@
 #include <sys/wait.h>
 
 int main() {
-    char* const argv[] =  {"cat", NULL};
-    char* const argv2[] = {"ls", NULL};
-    char* const argv3[] = {"less", NULL};
+    char* const argv1[] = {"find", "/", NULL};
+    char* const argv2[] = {"head", NULL};
+    char* const argv3[] = {"sleep", "3", NULL};
+    char* const argv4[] = {"head", NULL};
+    char* const argv5[] = {"sleep", "5", NULL};
 
-    execargs_t program1 = construct_execargs("cat", argv);
-    execargs_t program2 = construct_execargs("ls", argv2);
-    execargs_t program3 = construct_execargs("less", argv3);
-    execargs_t* programs[3] = {
+    execargs_t program1 = construct_execargs("find", argv1);
+    execargs_t program2 = construct_execargs("head", argv2);
+    execargs_t program3 = construct_execargs("sleep", argv3);
+    execargs_t program4 = construct_execargs("head", argv4);
+    execargs_t program5 = construct_execargs("sleep", argv5);
+
+    execargs_t* programs[5] = {
         &program1,
         &program2,
-        &program3
+        &program3,
+        &program4,
+        &program5
     };
 
-    runpiped(programs, 3); 
+    runpiped(programs, 5); 
+    fprintf(stderr, "after runpiped");
 
     int x;
     scanf("%d", &x);
